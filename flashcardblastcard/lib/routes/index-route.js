d0b455f684df2fc70a58be955d44d2d1f2e3725d
tripdragon <nickitdesign@gmail.com>
2024-04-07 14:54:20 -0400

    $(#*#%(

--- flashcardblastcard/lib/routes/index-route.js
+++ flashcardblastcard/lib/routes/index-route.js
@@ -4,6 +4,16 @@ module.exports = {
     method: 'GET',
     path: '/',
     options: {
-        handler: async (request, h) => {}
+        handler: async (request, h) => {
+          // return "sf,mdlfm!!";
+          return thedogandponytshow("neato");
+        }
     }
 };
+
+
+
+
+function thedogandponytshow(wow) {
+  return `<h2>${wow}</h2>`;
+}
