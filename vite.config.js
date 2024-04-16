05ef75e0282cbb4a2c992cdaabaa52f9d2b84a48
tripdragon <nickitdesign@gmail.com>
2024-04-16 11:31:29 -0400

    @&#^$

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  base: "http://tripdragon.github.io/flashcardblastcard",
   resolve : {
     alias: {
       '@utilites': path.resolve(__dirname, './src/utilites/utilites.js'),
