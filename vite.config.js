2eadb1094c6dd52b452664d440295cdb25f6a445
tripdragon <nickitdesign@gmail.com>
2024-04-16 20:22:08 -0400

    *$($*$

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "https://tripdragon.github.io/flashcardblastcard",
+  base: "https://tripdragon.github.io/flashcardblastcard/",
   // base: "/",
-  base: "/flashcardblastcard/",
+  // base: "/flashcardblastcard/",
   // base: './',
   resolve : {
     alias: {
