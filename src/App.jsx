ab5fea664f231988602f9b2205588f566dbad6ef
tripdragon <nickitdesign@gmail.com>
2024-04-16 21:10:04 -0400

    ^%*$&(#%$

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import {shuffle, randomInt} from '@utilites';
-
 function App() {
   const [count, setCount] = useState(0)
 
